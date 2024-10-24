a20990d2b332476cad6f7000573195c360a0d0ba
PritamMajumder-MFSI <pritamm@mindfiresolutions.com>
2024-10-24 17:37:08 +0530

    Logout api

--- src/controllers/authController/index.ts
+++ src/controllers/authController/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import signUpRoutes from "./signUp";
 import loginRoutes from "./login";
+import logoutRoutes from "./logout";
 const router = Router();
 
 router.use("/sign-up", signUpRoutes);
 router.use("/login", loginRoutes);
+router.use("/logout", logoutRoutes);
 
 export default router;
---
+++ src/controllers/authController/logout.ts
@@ -0,0 +1,17 @@
+import Router, { NextFunction, Request, Response } from "express";
+import { successResponse } from "../../utils";
+
+const router = Router();
+
+router.post("/", (request: Request, response: Response, next: NextFunction) => {
+  try {
+    response.clearCookie("accessToken");
+    response.clearCookie("refreshToken");
+
+    successResponse({ message: "Logout successful", data: {}, res: response });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
